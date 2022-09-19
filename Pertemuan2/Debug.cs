8f15eaa7e0a3a389146ac504940078fc26d280e0
Nur Ryan Dwi Cahyo <113457335+RyanDwi23@users.noreply.github.com>
2022-09-20 00:39:12 +0700

    Add files via upload

---
+++ Pertemuan2/Debug.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Debug : MonoBehaviour
+{
+    public string Ryan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Debug.Log("Hellow World"+Ryan);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
