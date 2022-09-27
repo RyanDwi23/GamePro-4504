f8f7d5ef4a1252a9c9d6ef0852d4f922a268aad3
Nur Ryan Dwi Cahyo <113457335+RyanDwi23@users.noreply.github.com>
2022-09-28 01:09:22 +0700

    Add files via upload

---
+++ Pertemuan4/ObjectMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 kekanan;
+    Vector3 kekiri;
+    Vector3 keatas;
+    Vector3 kebawah;
+    Vector3 maju;
+    Vector3 mundur;
+    public int aksi;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+            kekanan = new Vector3(1,0,0);
+            kekiri = new Vector3(-1,0,0);
+            keatas = new Vector3(0,1,0);
+            kebawah = new Vector3(0,-1,0);
+            maju = new Vector3(0,0,1);
+            mundur = new Vector3(0,0,-1);
+
+            void OnCollisionEnter2D(Collision2D coll){
+                if (coll.gameObject.tag == "EnemyTag"){
+                    UnityEngine.Debug.Log("Mulai Nabrak");
+                }
+            }
+            void OnCollisionStay2D(Collision2D coll){
+                if (coll.gameObject.tag == "EnemyTag"){
+                    UnityEngine.Debug.Log("Sedang Nabrak");
+                }
+            }
+            void OnCollisionExit2D(Collision2D coll){
+                if (coll.gameObject.tag == "EnemyTag"){
+                    UnityEngine.Debug.Log("Sudah Nabrak");
+                }
+            }
+            
+    }
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(aksi){
+            case 0:
+                transform.position = transform.position + (kekanan * 2 * Time.deltaTime);
+                break;
+                
+            case 1:
+                transform.position = transform.position + (kekiri * 2 * Time.deltaTime);
+                break;
+            
+            case 2:
+                transform.position = transform.position + (keatas * 2 * Time.deltaTime);
+                break;
+
+            case 3:
+                transform.position = transform.position + (kebawah * 2 * Time.deltaTime);
+                break;
+
+            case 4:
+                transform.rotation = transform.rotation * Quaternion.Euler (maju * 2 *Time.deltaTime);
+                break;
+
+            case 5:
+                transform.rotation = transform.rotation * Quaternion.Euler (mundur * 2 *Time.deltaTime);
+                break;
+        }
+    }
+}
